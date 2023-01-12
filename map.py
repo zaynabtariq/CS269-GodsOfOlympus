c4ec0afef147a2131ad9fc2d830de8da54b831e7
jctjad <122404431+jctjad@users.noreply.github.com>
2023-01-12 00:05:55 -0500

    Add files via upload

---
+++ map.py
@@ -0,0 +1,37 @@
+"""
+Gods of Olympus
+Last Modified: 1/11/23 by jctjad
+Course: CS269
+File: map.py
+"""
+
+import pygame
+
+class Map():
+    # Constructor
+    def __init__(self, HEIGHT, WIDTH, ACC, FRIC, FPS, screen):
+        self.HEIGHT = HEIGHT
+        self.WIDTH = WIDTH
+        self.ACC = ACC
+        self.fRIC = FRIC
+        self.FPS = FPS
+
+        # Set screen up
+        self.screen = screen
+        pygame.display.set_caption("Gods of Olympus")
+
+        # Load background
+        self.bg_image = pygame.image.load("OlympusBackground.jpg").convert_alpha()
+    
+    def draw_ledges(self):
+        ledge_1_rect = pygame.Rect((150, self.HEIGHT - 100, 200, 15))  # (left, top), (width, height)
+        ledge_1 = pygame.image.load( "platform1.png" ).convert_alpha()
+        ledge_1_scaled = pygame.transform.scale(ledge_1, (200, 180))
+        self.screen.blit(ledge_1_scaled, (150, 410))
+
+        return ledge_1, ledge_1_rect
+
+    # Methods
+    def draw_bg(self):
+        scaled_bg = pygame.transform.scale(self.bg_image, (self.WIDTH, self.HEIGHT))
+        self.screen.blit(scaled_bg, (0,0))
