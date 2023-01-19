f6e1e21d9e306f23056ed785019a807809868cf2
Alamport <108032137+Alamport@users.noreply.github.com>
2023-01-19 17:47:36 -0500

    Update fighter.py

--- fighter.py
+++ fighter.py
@@ -1,11 +1,12 @@
 """
 Gods of Olympus
-Last Modified: 1/17/23
+Last Modified: 1/19/23
 Course: CS269
 File: fighter.py
 """
 
 import pygame
+import random
 
 
 
@@ -78,7 +79,7 @@ class Fighter():
                         self.update_time = pygame.time.get_ticks()
                         self.attack(surface, target, self.attack_type)
                     if key[pygame.K_q]:
-                        self.action = 8
+                        self.action = 10
                         self.frame_index = 0
                         self.update_time = pygame.time.get_ticks()
                         self.attack_type = 3
@@ -132,10 +133,10 @@ class Fighter():
                 dy += self.vel_y
 
             # ensures the player stays on screen
-            if self.char.left + dx < 0:
-                dx = -self.char.left
-            if self.char.right + dx > WIDTH:
-                dx = WIDTH - self.char.right
+            if self.char.centerx + dx < 0:
+                dx = -self.char.centerx
+            if self.char.centerx + dx > WIDTH:
+                dx = WIDTH - self.char.centerx
             if self.char.bottom + dy > HEIGHT - 100:  # write - X if there is a floor
                 self.vel_y = 0
                 self.jump = False
@@ -144,6 +145,13 @@ class Fighter():
             # ensures players are facing each other
             if target.char.centerx > self.char.centerx:
                 self.flip = False
+                if self.player == 2:
+                    if self.action == 1:
+                        self.action = 0
+                    if self.action == 5:
+                        self.action = 4
+                    if self.action == 7:
+                        self.action = 6
 
             else:
                 self.flip = True
@@ -152,12 +160,10 @@ class Fighter():
                         self.action = 1
                     if self.action == 4:
                         self.action = 5
-                    if self.action == 6:
-                        self.action = 7
 
             # collision with ledges
             for ledge in ledges:
-                if self.char.x >= ledge[0] and self.char.x <= (ledge[0] + ledge[2]):
+                if self.char.x >= ledge[0] - 190 and self.char.x <= (ledge[0] + ledge[2]) - 190:
                     if self.char.bottom <= ledge[1]:
                         self.vel_y = 0
                         #self.jump = False
@@ -166,10 +172,12 @@ class Fighter():
                             if key[pygame.K_s]:
                                 self.vel_y = 30
                                 dy = HEIGHT - self.char.bottom - 100
+                                self.jump = False
                         elif self.player == 2:
                             if key[pygame.K_DOWN]:
                                 self.vel_y = 30
                                 dy = HEIGHT - self.char.bottom - 100
+                                self.jump = False
 
             # update player position
             self.char.x += dx
@@ -181,7 +189,7 @@ class Fighter():
         if type == 1:
             attacking_rect = pygame.Rect(self.char.centerx - (3.5 * self.char.width * self.flip), self.char.y,
                                          3.5 * self.char.width, self.char.height)
-            if attacking_rect.colliderect(target.char):
+            if attacking_rect.colliderect(target.char.centerx):
                 target.health -= 2
                 if self.flip == False:
                     target.char.x += 5
@@ -190,10 +198,9 @@ class Fighter():
                 # add a timer and make attacking false after 2 secs
             self.attacking = False
 
-    def random_melee():
-       attack_damage = random.randint(1,3) 
-       return attack_damage
-
+    def random_melee(self):
+        attack_damage = random.randint(1, 3)
+        return attack_damage
 
     def update_action(self, new_action):
         # check if the new action is different to the previous one
@@ -205,7 +212,3 @@ class Fighter():
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.char)
-
-
-
-    
