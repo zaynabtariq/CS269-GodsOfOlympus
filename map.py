275578b0b273a5b70baa67f8f599b89c4674f421
Alamport <108032137+Alamport@users.noreply.github.com>
2023-01-19 17:48:17 -0500

    Update map.py

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 """
 Gods of Olympus
-Last Modified: 1/15/23 by zaynab
+Last Modified: 1/19/23 
 Course: CS269
 File: map.py
 """
@@ -24,11 +24,11 @@ class Map():
 
         # Load background
         if self.num == 1:
-            self.bg_image = pygame.image.load("skybackground_2.png").convert_alpha()
+            self.bg_image = pygame.image.load("Images/skybackground_2.png").convert_alpha()
         elif self.num == 2:
-            self.bg_image = pygame.image.load("sky.png").convert_alpha()
+            self.bg_image = pygame.image.load("Images/sky.png").convert_alpha()
         elif self.num == 3:
-            self.bg_image = pygame.image.load("sky.png").convert_alpha()
+            self.bg_image = pygame.image.load("Images/sky.png").convert_alpha()
 
     # colors
     RED = (255, 0, 0)
@@ -44,13 +44,13 @@ class Map():
     def draw_ledges(self):
          if self.num == 1:
             ledge_1_rect = pygame.Rect((155, self.HEIGHT - 300, 260, 40))  # (left, top), (width, height)
-            ledge_1 = pygame.image.load("sky_platform.png").convert_alpha()
+            ledge_1 = pygame.image.load("Images/sky_platform.png").convert_alpha()
             ledge_1_scaled = pygame.transform.scale(ledge_1, (300, 100))
             self.screen.blit(ledge_1_scaled, (150, 410))
 
 
             ledge_2_rect = pygame.Rect((self.WIDTH - 485, self.HEIGHT - 350, 255, 40))  # (left, top), (width, height)
-            ledge_2 = pygame.image.load("sky_platform.png").convert_alpha()
+            ledge_2 = pygame.image.load("Images/sky_platform.png").convert_alpha()
             ledge_2_scaled = pygame.transform.scale(ledge_2, (300, 100))
             self.screen.blit(ledge_2_scaled, (self.WIDTH - 500, 360))
 
@@ -73,9 +73,20 @@ class Map():
 
         self.scroll -= 1
 
-        ground = pygame.image.load("sky_floor.png").convert_alpha()
+        ground = pygame.image.load("Images/sky_floor.png").convert_alpha()
         ground_scaled = pygame.transform.scale(ground, (self.WIDTH + 10, 110))
         self.screen.blit(ground_scaled, (0, self.HEIGHT -110))
+
+
+    # draw icons next to scoreboard
+
+    def draw_score_icons(self, screen):
+        fighter1_icon = pygame.image.load("Images/Icons_Zeus.png")
+        screen.blit(fighter1_icon, (30,30))
+
+        fighter2_icon = pygame.image.load("Images/Icons_Hades.png")
+        screen.blit(fighter2_icon, (1170,30))
+
 
     # draws health bars
 
@@ -86,17 +97,17 @@ class Map():
         pygame.draw.rect(screen, (255, 255, 0), (x, y, 400 * ratio, 30))'''
         #scoreboards
         if player == 'fighter_2':
-            left_blue_inner = pygame.image.load("left_blue_inner.jpg").convert_alpha()
+            left_blue_inner = pygame.image.load("Images/left_blue_inner.jpg").convert_alpha()
             scaled_isb2 = pygame.transform.scale(left_blue_inner, (400, 50))
             screen.blit(scaled_isb2, (150,60))
-            left_green_outer = pygame.image.load("left_green_outer.jpg").convert_alpha()
+            left_green_outer = pygame.image.load("Images/left_green_outer.jpg").convert_alpha()
             scaled_osb = pygame.transform.scale(left_green_outer, (400 * ratio, 50))
             screen.blit(scaled_osb, (150 + (400 - 400 * ratio),60))
 
         if player == 'fighter_1':
-            right_red_inner = pygame.image.load("right_red_inner.jpg").convert_alpha()
+            right_red_inner = pygame.image.load("Images/right_red_inner.jpg").convert_alpha()
             scaled_isb = pygame.transform.scale(right_red_inner, (400, 50))
             screen.blit(scaled_isb, (750,60))
-            right_purple_outer = pygame.image.load("right_purple_outer.jpg").convert_alpha()
+            right_purple_outer = pygame.image.load("Images/right_purple_outer.jpg").convert_alpha()
             scaled_osb2 = pygame.transform.scale(right_purple_outer, (400 * ratio, 50))
             screen.blit(scaled_osb2, (750, 60))
